62e8d348beb6d5b6adbdbeaee0342676be717723
Karina Ramadhan <carinasofyan@gmail.com>
2019-02-11 20:00:41 +0800

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-concentric-25085',
-    user : 'karinasofyan',
-    password : '',
-    database : 'smartbrain'
+    connectionString : process.env.DATABASE_URL,
+    ssl:true,
   }
 });
 
