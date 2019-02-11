681094e32c7271beccea74d114186c45711a0da0
Karina Ramadhan <carinasofyan@gmail.com>
2019-02-11 19:54:50 +0800

    addint database to heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-concentric-25085',
     user : 'karinasofyan',
     password : '',
     database : 'smartbrain'
